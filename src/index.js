335ac37ed8fe2c8fe18ff52b8353983d02a90a8a
konglong <1016361268@qq.com>
2022-06-04 03:31:02 +0800

    子类继承父类

---
+++ src/index.js
@@ -0,0 +1,25 @@
+父类
+class People {
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+  }
+  eat() {
+    alert(`${this.name} eat something`)
+  }
+  speak() {
+    alert(`My name is ${this.name}, age ${this.age}`)
+  }
+}
+
+// 子类 继承父类
+class Student extends People {
+  constructor(name, age, number) {
+    super(name, age)
+    this.number = number
+  }
+  study() {
+    alert(`${this.name} study`)
+  }
+}
+
---
+++ webpack.dev.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname,
+    filename: './release/bundle.js'
+  },
+  module: {
+    rules: [      // js语法转换
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
+  devServer: {
+    // contentBase: path.join(__dirname, './release'),
+    open: true,
+    port: 9000
+  }
+}
