6659b8add08ca5317ea4c0402ecdb642efe6a009
konglong <1016361268@qq.com>
2022-06-04 15:16:05 +0800

    面向对象应用举例

--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
-class People {
-  constructor(name) {
-    this.name = name
+class jQuery {
+  constructor(selector) {
+    let slice = Array.prototype.slice
+    let dom = slice.call(document.querySelectorAll(selector))
+    let len = dom ? dom.length : 0
+    for (let i = 0; i < len; i++) {
+      this[i] = dom[i]
+    }
+    this.length = len
+    this.selector = selector || ''
   }
-  saySomething() {
+  append(node) {
+
+  }
+  addClass(name) {
+
+  }
+  html(data) {
 
   }
 }
 
-class A extends People {
-  constructor(name) {
-    super(name)
-  }
-  saySomething() {
-    console.log('I am A')
-  }
+window.$ = function (selector) {
+  return new jQuery(selector)
 }
 
-class B extends People {
-  constructor(name) {
-    super(name)
-  }
-  saySomething() {
-    console.log('I am b')
-  }
-}
-
-let a = new A('a')
-a.saySomething()
-let b = new B('b')
-b.saySomething()
+// 测试代码
+var $p = $('p')
+console.log($p)
+console.log($p.addClass)
+//  NodeList 节点实例很像数组,可以使用length属性和forEach方法。但是,它不是数组,不能使用pop或push之类数组特有的方法
+console.log(document.querySelectorAll('p'))
