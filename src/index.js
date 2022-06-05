efddd143e3b970682fea18aa7a90fdf49c19d765
konglong <1016361268@qq.com>
2022-06-05 14:24:46 +0800

    ES7带参数的装饰器

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-@testDec
+// ES7 带参数的装饰器
+function testDec(isDec) { // 参数接受
+  return function (target) { // 返回函数默认接受的是一个类，所以参数不能在这里接收
+    target.isDec = isDec
+  }
+}
+
+// ES7 装饰类
+@testDec(false)
 class Demo {
 
 }
 
-function testDec(target) {
-  target.isDec = true
-}
 alert(Demo.isDec)
