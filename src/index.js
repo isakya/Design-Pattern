a596c856cdc208a43ecc45c8ad60fa494a3a9da8
konglong <1016361268@qq.com>
2022-06-06 17:17:16 +0800

    迭代器代码实现

--- src/index.js
+++ src/index.js
@@ -1,49 +1,37 @@
-// 观察者模式
-
-// 主题，保存状态，状态变化之后触发所有观察者对象
-class Subject {
-  constructor() {
-    this.state = 0
-    this.observers = []
+// 迭代器模式代码实现
+class Iterator {
+  constructor(container) {
+    this.list = container.list
+    this.index = 0
   }
-  // 获取当前主题
-  getState() {
-    return this.state
+  next() {
+    if (this.hasNext()) {
+      return this.list[this.index++]
+    }
+    return null
   }
-  // 主题更新，调用通知所有订阅者
-  setState(state) {
-    this.state = state
-    this.notifyAllObservers()
-  }
-  // 通知所有订阅者
-  notifyAllObservers() {
-    this.observers.forEach(observer => {
-      observer.update()
-    })
-  }
-  // 添加订阅者
-  attach(observer) {
-    this.observers.push(observer)
+  hasNext() {
+    if (this.index >= this.list.length) {
+      return false
+    }
+    return true
   }
 }
 
-// 观察者
-class Observer {
-  constructor(name, subject) {
-    this.name = name
-    this.subject = subject
-    this.subject.attach(this)
+class Container {
+  constructor(list) {
+    this.list = list
   }
-  update() {
-    console.log(`${this.name} update, state: ${this.subject.getState()}`)
+  // 生成遍历器
+  getItertor() {
+    return new Iterator(this)
   }
 }
 
-// 测试
-let s = new Subject()
-let o1 = new Observer('o1', s)
-let o2 = new Observer('o2', s)
-let o3 = new Observer('o3', s)
+var arr = [1, 2, 3, 4, 5]
 
-// 更新主题
-s.setState(1)
+let container = new Container(arr)
+let iterator = container.getItertor()
+while (iterator.hasNext()) {
+  console.log(iterator.next())
+}
