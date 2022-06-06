a20071b5b099dd4d1098287ddd56ea7bd2cc8dcc
konglong <1016361268@qq.com>
2022-06-06 19:06:37 +0800

    状态模式

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-function each(data) {
-  // 带有遍历器特性的对象 就是 data[Symbol.iterator] 有值 才能用 for ... of
-  // for ... of 迭代器语法糖
-  for (let item of data) {
-    console.log(item)
+// 状态(红灯、绿灯、黄灯)
+class State {
+  constructor(color) {
+    this.color = color
+  }
+  handle(context) {
+    console.log(`turn to ${this.color} light`)
+    // 设置状态
+    context.setState(this)
   }
 }
 
-let a = '12345'
-let arr = [1, 2, 3, 4]
-let m = new Map()
-m.set('a', 100)
-m.set('b', 100)
+// 主体
+class Context {
+  constructor() {
+    this.state = null
+  }
+  // 获取状态
+  getState() {
+    return this.state
+  }
+  setState(state) {
+    this.state = state
+  }
+}
 
-each(a)
-each(m)
-each(arr)
+// test
+let context = new Context()
+
+let green = new State('green')
+let red = new State('red')
+let yellow = new State('yellow')
+
+// 绿灯亮了
+green.handle(context)
+// 打印状态
+console.log(context.getState())
+
+
+// 核心：
+// 状态的切换 和 状态的获取是分开的，在逻辑上做了个分离
