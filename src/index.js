c6f3b0fe9334aea64489807151c96fe369a9f5a7
konglong <1016361268@qq.com>
2022-06-06 18:47:38 +0800

    for ... of 迭代器语法糖

--- src/index.js
+++ src/index.js
@@ -1,25 +1,8 @@
 function each(data) {
-  // 生成遍历器
-  let iterator = data[Symbol.iterator]()
-  // data[Symbol.iterator]().next()注意不能直接next
-  // 这样只会重复拿到 index 0 的值
-
-  let item = { done: false }
-
-  // b.next()
-  // { value: 'a', done: false }
-  // b.next()
-  // { value: 'b', done: false }
-  // b.next()
-  // { value: 'c', done: false }
-  // b.next()
-  // { value: undefined, done: true } // 没有数据时返回
-
-  while (!item.done) {
-    item = iterator.next()
-    if (!item.done) {
-      console.log(item.value)
-    }
+  // 带有遍历器特性的对象 就是 data[Symbol.iterator] 有值 才能用 for ... of
+  // for ... of 迭代器语法糖
+  for (let item of data) {
+    console.log(item)
   }
 }
 
